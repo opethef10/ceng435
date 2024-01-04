a7701fa745d3b9c508ca010e7a50d73ce95e1bda
Onur Arıkan <onur93arikan@gmail.com>
2024-01-04 03:17:46 +0300

    udp update

--- code/udp/udp_client.py
+++ code/udp/udp_client.py
@@ -1,43 +1,96 @@
 import socket
 import os
 
-IP_ADDRESS = "0.0.0.0"
-PORT = 45404
-OBJECTS_DIR = "./test_obj/"
+SERVER_IP_ADDRESS = "172.17.0.2"
+SERVER_PORT = 45404
+OBJECTS_DIR = "/root/objects/"
 
 BUFFER_SIZE = 2048  # Adjust the buffer size according to your needs
 
+def receive_and_save_objects(client_socket, objects_dir):
+    # try:
+        received_data = b""
+        left, mid, right = b"", b"", b""
 
-def udp_client(file_name):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        for i in range(10):
+            completed = False
+            for size in "large", "small":
+                received_data = right
+                while True:
+                    chunk, _ = client_socket.recvfrom(1024)
+                    if not chunk:
+                        print("break", size, i)
+                        completed = True
+                        break
+                    left, mid, right = chunk.partition(b"=\n")
+                    if not mid:
+                        received_data += chunk
+                    else:
+                        received_data += left + mid
+                        print(size,i,len(received_data))
+                        break
 
-    server_address = (IP_ADDRESS, PORT)
+                if not os.path.exists(objects_dir):
+                    os.makedirs(objects_dir)
 
-    # Send the file name to the server
-    client_socket.sendto(file_name.encode(), server_address)
+                file_path = os.path.join(objects_dir, f"received_{size}-{i}.obj")
 
-    file_path = os.path.join(OBJECTS_DIR, file_name)
+                try:
+                    with open(file_path, "wb") as file:
+                        file.write(received_data)
+                    print(f"Received and saved {size} Object {i} to {file_path}")
 
-    try:
-        with open(file_path, "wb") as file:
-            while True:
-                chunk, _ = client_socket.recvfrom(BUFFER_SIZE)
-                if not chunk:
-                    break
-                if b"=" in chunk:
-                    file.write(chunk)
-                    break
-                file.write(chunk)
+                except Exception as e:
+                    print(f"Error saving {size} Object {i}: {e}")
 
-        print(f"Received and saved {file_name} to {file_path}")
+    # except Exception as e:
+    #     print(f"An error occurred: {e}")
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket:
+        # try:
+            ready_message = b"Ready"
+            client_socket.sendto(ready_message, (SERVER_IP_ADDRESS, SERVER_PORT))
+            print("UDP Client is ready...")
+            receive_and_save_objects(client_socket, OBJECTS_DIR)
 
-    finally:
-        client_socket.close()
+        # except Exception as e:
+        #     print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
-    file_name = "small-1.obj"  # Specify the file to be requested
-    udp_client(file_name)
+    main()
+
+# def udp_client(file_name):
+#     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#
+#     server_address = (IP_ADDRESS, PORT)
+#
+#     # Send the file name to the server
+#     client_socket.sendto(file_name.encode(), server_address)
+#
+#     file_path = os.path.join(OBJECTS_DIR, file_name)
+#
+#     try:
+#         with open(file_path, "wb") as file:
+#             while True:
+#                 chunk, _ = client_socket.recvfrom(BUFFER_SIZE)
+#                 if not chunk:
+#                     break
+#                 if b"=" in chunk:
+#                     file.write(chunk)
+#                     break
+#                 file.write(chunk)
+#
+#         print(f"Received and saved {file_name} to {file_path}")
+#
+#     except Exception as e:
+#         print(f"An error occurred: {e}")
+#
+#     finally:
+#         client_socket.close()
+#
+#
+# if __name__ == "__main__":
+#     file_name = "small-1.obj"  # Specify the file to be requested
+#     udp_client(file_name)
--- code/udp/udp_server.py
+++ code/udp/udp_server.py
@@ -1,54 +1,103 @@
 import socket
 import os
+import time
 
-IP_ADDRESS = "0.0.0.0"
+IP_ADDRESS = "172.17.0.2"
 PORT = 45404
-# OBJECTS_DIR = "/root/objects/"
-OBJECTS_DIR = "../../objects/"
+OBJECTS_DIR = "/root/objects/"
+EXPERIMENT_LOG_FILE = "server_experiment_log.txt"
 
 BUFFER_SIZE = 1024  # Adjust the buffer size according to your needs
 
+def main():
+    # try:
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            server_socket.bind((IP_ADDRESS, PORT))
 
-def udp_server():
-    # Create a UDP socket
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            # Wait for the "ready" message from the client
+            print("Waiting for the client to be ready...")
+            data, client_address = server_socket.recvfrom(1024)
+            print(f"Received ready message from {client_address}")
 
-    # Set the socket options to allow quick reuse of the socket
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+            start_time = time.time()
 
-    # Bind the socket to the IP address and port
-    server_socket.bind((IP_ADDRESS, PORT))
+            # connection, client_address = server_socket.accept()
+            # print(f"Connection from {client_address}")
 
-    print(f"UDP server is listening on {IP_ADDRESS}:{PORT}")
+            for i in range(10):
+                for size in "large", "small":
+                    file_path = os.path.join(OBJECTS_DIR, f"{size}-{i}.obj")
 
-    while True:
-        # Receive data from the client
-        # The UDP socket will wait here to receive data from the client in idle state
-        data, client_address = server_socket.recvfrom(BUFFER_SIZE)
-        
-        # Get the file name from the received data
-        file_name = data.decode()
+                    if not os.path.exists(file_path):
+                        print(f"Error: File not found - {file_path}")
+                        continue
 
-        # Construct the file path
-        file_path = os.path.join(OBJECTS_DIR, file_name)
+                    with open(file_path, "rb") as file:
+                        while True:
+                            chunk = file.read(BUFFER_SIZE)
+                            if not chunk:
+                                break
+                            server_socket.sendto(chunk, client_address)
+                    # with open(file_path, "rb") as file:
+                    #     data = file.read()
+                    #
+                    # server_socket.sendto(data, client_address)
+                    print(f"Sent {size} Object {i}")
 
-        if os.path.exists(file_path):
-            with open(file_path, "rb") as file:
-                # Send the file to the client chunk by chunk
-                while True:
-                    chunk = file.read(BUFFER_SIZE)
-                    if not chunk:
-                        break
-                    # print(chunk[-10:])
-                    server_socket.sendto(chunk, client_address)
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            with open(EXPERIMENT_LOG_FILE, "a") as log_file:
+                log_file.write(f"Experiment x: {elapsed_time} seconds\n")
 
-                print(f"Sent {file_name} to {client_address}")
-        else:
-            error_message = f"Error: File not found - {file_path}"
-            server_socket.sendto(error_message.encode(), client_address)
-            print(error_message)
-            break  # Stop the server after handling one file
+    # except Exception as e:
+    #     print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
-    udp_server()
+    main()
+
+
+# def udp_server():
+#     # Create a UDP socket
+#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#
+#     # Set the socket options to allow quick reuse of the socket
+#     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+#
+#     # Bind the socket to the IP address and port
+#     server_socket.bind((IP_ADDRESS, PORT))
+#
+#     print(f"UDP server is listening on {IP_ADDRESS}:{PORT}")
+#
+#     while True:
+#         # Receive data from the client
+#         # The UDP socket will wait here to receive data from the client in idle state
+#         data, client_address = server_socket.recvfrom(BUFFER_SIZE)
+#
+#         # Get the file name from the received data
+#         file_name = data.decode()
+#
+#         # Construct the file path
+#         file_path = os.path.join(OBJECTS_DIR, file_name)
+#
+#         if os.path.exists(file_path):
+#             with open(file_path, "rb") as file:
+#                 # Send the file to the client chunk by chunk
+#                 while True:
+#                     chunk = file.read(BUFFER_SIZE)
+#                     if not chunk:
+#                         break
+#                     # print(chunk[-10:])
+#                     server_socket.sendto(chunk, client_address)
+#
+#                 print(f"Sent {file_name} to {client_address}")
+#         else:
+#             error_message = f"Error: File not found - {file_path}"
+#             server_socket.sendto(error_message.encode(), client_address)
+#             print(error_message)
+#             break  # Stop the server after handling one file
+#
+#
+# if __name__ == "__main__":
+#     udp_server()
