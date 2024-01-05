073f0b594ccfc7f5fbe81914b4043b05799faad3
Onur Arıkan <onur93arikan@gmail.com>
2024-01-05 21:42:22 +0300

    write comments

--- code/udp/udp_client.py
+++ code/udp/udp_client.py
@@ -5,16 +5,15 @@ import sys
 
 SERVER_IP_ADDRESS = "172.17.0.2"
 SERVER_PORT = 45404
-OBJECTS_DIR = "/app/udp/objects/"
+OBJECTS_DIR = "/root/objects/"
 
 WINDOW_SIZE = 100
 
-BUFFER_SIZE = 2048  # Adjust the buffer size according to your needs
+BUFFER_SIZE = 2048
 SEQUENCE_NUMBER_SIZE = 4
 CHECKSUM_SIZE = 16
 
-
-
+# Comment out next line if you wanna see prints for debug purpose
 sys.stdout = open("/dev/null", "w")
 
 
@@ -32,20 +31,21 @@ def send_acknowledgment(client_socket, ack_number):
 
 
 def receive_and_save_objects(client_socket, objects_dir):
-    # try:
-    received_data = b""
-    left, mid, right = b"", b"", b""
+
+    # We have a window of received objects
     received_dict = {}
 
     global corrupted_count
 
     while True:
+        # Message comes from the buffer and header and data is separated by fixed sizes
         message, _ = client_socket.recvfrom(BUFFER_SIZE)
         sequence_number = int(message[:SEQUENCE_NUMBER_SIZE])
         checksum = message[SEQUENCE_NUMBER_SIZE : SEQUENCE_NUMBER_SIZE + CHECKSUM_SIZE]
         chunk = message[SEQUENCE_NUMBER_SIZE + CHECKSUM_SIZE :]
-        # sequence_number, chunk, checksum = message.split(HEADER_SEPARATOR.encode())
 
+        # We compare the packets' checksum and if they are not equal
+        # Server needs the resend the packet because of no ACK
         client_checksum = calculate_checksum(chunk)
 
         if not client_checksum == checksum:
@@ -53,18 +53,27 @@ def receive_and_save_objects(client_socket, objects_dir):
             corrupted_count += 1
             continue
 
-        # sequence_number = sequence_number.decode()
-        # chunk = chunk.encode()
         print("receive", sequence_number, len(chunk))
         if sequence_number not in received_dict:
+            # In case of non-duplicate sequence, we update the received sequence
+            # This is also for tagging and sorting purposes
+            # At the end, packets will be in correct order
             received_dict[sequence_number] = chunk
         else:
+            # Otherwise it means it's duplicate
             print("duplicate", sequence_number)
         send_acknowledgment(client_socket, sequence_number)
 
         print(len(received_dict), int(sequence_number) + 1)
 
         if b"".join(received_dict.values()).count(b"=\n") == 20:
+            # We found out that the files are ending with = and new line
+            # If we have 20 of them this means we transfered all objects
+            # But there is an edge case which is that the last part of object came faster
+            # than previous sequences. The one having the last = must have the highest sequence
+            # number so we use this information so that when our dict has a length one higher than
+            # the max_key (because seq num starts from 0, that makes sense)
+            # We are ready to break the loop
             max_key = max(received_dict.keys())
             print(len(received_dict), int(sequence_number) + 1)
             # print(sorted(received_dict.keys()))
@@ -73,15 +82,20 @@ def receive_and_save_objects(client_socket, objects_dir):
                 completed = True
                 break
 
+    # And finally we sort the key according to their tags, so that the data is in correct order
     received_data = b"".join(received_dict[key] for key in sorted(received_dict.keys()))
     print(len(received_data))
 
+    # We separate each file accourding to their endings
     file_datas = [file + b"=\n" for file in received_data.split(b"=\n")][:-1]
 
     for idx, file_data in enumerate(file_datas):
+        # This is a trick which we determine the object number and whether it's small or large
         file_idx, size_idx = divmod(idx, 2)
         size = "small" if size_idx else "large"
 
+        # The rest covers the writing the file to the client's docker
+        # We checked the file checksums with linux's md5sum command and they work
         if not os.path.exists(objects_dir):
             os.makedirs(objects_dir)
 
@@ -98,7 +112,7 @@ def receive_and_save_objects(client_socket, objects_dir):
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket:
-        # try:
+        # Client initiates the first message so that server starts sending
         ready_message = b"Ready"
         client_socket.sendto(ready_message, (SERVER_IP_ADDRESS, SERVER_PORT))
         print("UDP Client is ready...")
--- code/udp/udp_server.py
+++ code/udp/udp_server.py
@@ -4,6 +4,7 @@ import time
 import hashlib
 import sys
 
+# Comment out next line if you wanna see prints for debug purpose
 sys.stdout = open("/dev/null", "w")
 
 IP_ADDRESS = "172.17.0.2"
@@ -11,15 +12,9 @@ PORT = 45404
 OBJECTS_DIR = "/root/objects/"
 EXPERIMENT_LOG_FILE = "server_experiment_log.txt"
 
-# IP_ADDRESS = "0.0.0.0"
-# PORT = 45404
-# OBJECTS_DIR = "../../objects/"
-
-
-
 WINDOW_SIZE = 100
 
-BUFFER_SIZE = 2048  # Adjust the buffer size according to your needs
+BUFFER_SIZE = 2048
 CHECKSUM_SIZE = 16
 SEQUENCE_NUMBER_SIZE = 4
 TIMEOUT = 0.15
@@ -33,15 +28,16 @@ def calculate_checksum(data):
 def send_data_with_reliability(server_socket, sequence_number, data, client_address):
     print("send", sequence_number, len(data))
     checksum = calculate_checksum(data)
+
+    # 4 bytes of sequence number
+    # 16 bytes of checksum
+    # The rest is data
+
     message = (
             f"{sequence_number:04}".encode() +
-            # HEADER_SEPARATOR.encode() +
             checksum +
             data
-            # HEADER_SEPARATOR.encode() +
-            # checksum
     )
-    # f"{sequence_number:04}{HEADER_SEPARATOR}{data.decode()}".encode()
     server_socket.sendto(
         message,
         client_address,
@@ -64,55 +60,77 @@ def send_file(server_socket, file_path, client_address):
         chunk = b''
         while True:
             while len(window) < WINDOW_SIZE:
+                # Sequence number is 4 bytes
+                # MD5 checksum takes 16 bytes
+                # The header length is 20 bytes, we subtract it from the buffer size
+                # So that we read bytes from the file in the right amount
                 chunk = file.read(BUFFER_SIZE - SEQUENCE_NUMBER_SIZE - CHECKSUM_SIZE)
-                # time.sleep(0.001)
+
                 if not chunk:
+                    # In case of the file finished, get out of the selective repeat moving window
                     break
                 not_acked_counter = 0
+
+                # We assign the sequence number to the data, so in case of data loss, we will use this
+                # construction to resend the data again
                 window[sequence_number] = chunk
+
+                # We add sequence numbers to the sent_not_acked set, we will remove the ones which are acked from the set
                 sent_not_acked.add(sequence_number)
+
+                # Send the data
                 send_data_with_reliability(
                     server_socket, sequence_number, chunk, client_address
                 )
+
+                # Increment the sequence number after sending the data
                 sequence_number += 1
 
+            # Receive acknowledgements from the client
             ack_numbers = receive_acknowledgments(server_socket, window)
+
+            # remove ack numbers from the sent_not_acked set
             sent_not_acked.difference_update(ack_numbers)
             
             print("ack", ack_numbers)
 
 
             for ack in ack_numbers:
-                if (ack) in acked_table:
-                    acked_table[(ack)] = 2
+                if ack in acked_table:
+                    # this is like an enum for duplicate ACKs
+                    acked_table[ack] = 2
                 else:
-                    acked_table[(ack)] = 1
+                    # this represents ACK which isn't duplicate
+                    acked_table[ack] = 1
 
             for ack_number in ack_numbers:
-                # if ack_number not in window and ack_number in filtered_list:
-                #     continue
-                if ack_number not in window and acked_table[(ack_number)] > 1:
+                if ack_number not in window and acked_table[ack_number] > 1:
+                    # If ACK is removed from the window but a duplicate ACK came from the client
+                    # Discard the duplicate ACK
                     continue
+                # Otherwise, remove the ACK'd packet from the window
                 del window[ack_number]
-                # if ack_number not in window and ack_number in acked_table:
-                #     continue
 
             if not window and not chunk:
+                # In case of window is cleared and the file is finished, leave the loop
                 print("window chunk break")
                 break
 
             for seq in sent_not_acked:
                 if not_acked_counter <= 150:
+                    # Sometimes client leaves and server has one last duplicate packet sending
+                    # We covered this by giving a threshold which is reasonably high and more
+                    # than the window size
                     print("resend", seq)
                     counter += 1
+
+                    # In case of msg is not ACK'd after the timeout, we resend the messages
                     send_data_with_reliability(
                         server_socket, seq, window[seq], client_address
                     )
                     not_acked_counter += 1
                 else:
                     return
-
-            # time.sleep(TIMEOUT)
 
 
 def receive_acknowledgments(server_socket, window):
@@ -121,10 +139,13 @@ def receive_acknowledgments(server_socket, window):
 
     try:
         while True:
+            # Sequence number is max 9999, so 4 bytes is enough
+            # We fetch ACK number for 4 bytes, so we add each one separately
             data, _ = server_socket.recvfrom(SEQUENCE_NUMBER_SIZE)
             ack_numbers.add(int(data.decode()))
 
     except socket.timeout:
+        # In case of timeout, the ack number isn't added to the set
         pass
 
     return ack_numbers
@@ -132,20 +153,21 @@ def receive_acknowledgments(server_socket, window):
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
+        # Initialize UDP socket
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # Bind the server socket to given IP address and the port number
         server_socket.bind((IP_ADDRESS, PORT))
 
         # Wait for the "ready" message from the client
         print("Waiting for the client to be ready...")
         data, client_address = server_socket.recvfrom(BUFFER_SIZE)
         print(f"Received ready message from {client_address}")
-        
+
+        # We had a mixture of ready message and the ACKs, so we start sending after 100 ms
         time.sleep(0.1)
 
+        # We start the timer after everything is ready
         start_time = time.time()
-
-        # connection, client_address = server_socket.accept()
-        # print(f"Connection from {client_address}")
 
         for i in range(10):
             for size in "large", "small":
@@ -161,10 +183,10 @@ def main():
         elapsed_time = end_time - start_time
 
         print(f"Counter: {counter}")
+
+        # We write the timing results to our log file
         with open(EXPERIMENT_LOG_FILE, "a") as log_file:
             log_file.write(f"Experiment x: {elapsed_time} seconds\n")
-
-
 
 
 if __name__ == "__main__":
