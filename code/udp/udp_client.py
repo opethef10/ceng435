bcdc87fd0b59781f8d9f7c64c27218636ea885b4
basar-yilmaz <basaryilmaz.tr@outlook.com>
2024-01-05 20:57:03 +0300

    last update

--- code/udp/udp_client.py
+++ code/udp/udp_client.py
@@ -1,50 +1,30 @@
 import hashlib
 import socket
 import os
+import sys
 
 SERVER_IP_ADDRESS = "172.17.0.2"
 SERVER_PORT = 45404
 OBJECTS_DIR = "/app/udp/objects/"
 
-WINDOW_SIZE = 5
+WINDOW_SIZE = 100
 
-BUFFER_SIZE = 1024  # Adjust the buffer size according to your needs
+BUFFER_SIZE = 2048  # Adjust the buffer size according to your needs
 SEQUENCE_NUMBER_SIZE = 4
 CHECKSUM_SIZE = 16
-HEADER_SEPARATOR = ":"
+
+
+
+sys.stdout = open("/dev/null", "w")
+
+
+corrupted_count = 0
 
 
 def calculate_checksum(data):
     md5_hash = hashlib.md5()
     md5_hash.update(data)
     return md5_hash.digest()
-
-
-def receive_data_with_reliability(client_socket, expected_sequence_number):
-    while True:
-        data, _ = client_socket.recvfrom(BUFFER_SIZE)
-        sequence_number, chunk = data.decode().split(HEADER_SEPARATOR)
-
-        if int(sequence_number) == expected_sequence_number:
-            return chunk, expected_sequence_number
-        else:
-            print(
-                f"Received out-of-order packet. Expected: {expected_sequence_number}, Received: {sequence_number}"
-            )
-
-
-def receive_file(client_socket):
-    expected_sequence_number = 0
-
-    while True:
-        chunk, sequence_number = receive_data_with_reliability(
-            client_socket, expected_sequence_number
-        )
-
-        with open(OBJECTS_DIR + f"received_{sequence_number}.obj", "ab") as file:
-            file.write(chunk)
-
-        expected_sequence_number += 1
 
 
 def send_acknowledgment(client_socket, ack_number):
@@ -57,19 +37,22 @@ def receive_and_save_objects(client_socket, objects_dir):
     left, mid, right = b"", b"", b""
     received_dict = {}
 
+    global corrupted_count
+
     while True:
-        message, _ = client_socket.recvfrom(1024)
+        message, _ = client_socket.recvfrom(BUFFER_SIZE)
         sequence_number = int(message[:SEQUENCE_NUMBER_SIZE])
-        checksum = message[SEQUENCE_NUMBER_SIZE: SEQUENCE_NUMBER_SIZE + CHECKSUM_SIZE]
-        chunk = message[SEQUENCE_NUMBER_SIZE + CHECKSUM_SIZE:]
+        checksum = message[SEQUENCE_NUMBER_SIZE : SEQUENCE_NUMBER_SIZE + CHECKSUM_SIZE]
+        chunk = message[SEQUENCE_NUMBER_SIZE + CHECKSUM_SIZE :]
         # sequence_number, chunk, checksum = message.split(HEADER_SEPARATOR.encode())
 
         client_checksum = calculate_checksum(chunk)
 
         if not client_checksum == checksum:
             print("corrupted", sequence_number)
+            corrupted_count += 1
             continue
-        
+
         # sequence_number = sequence_number.decode()
         # chunk = chunk.encode()
         print("receive", sequence_number, len(chunk))
@@ -81,15 +64,14 @@ def receive_and_save_objects(client_socket, objects_dir):
 
         print(len(received_dict), int(sequence_number) + 1)
 
-        if b"".join(received_dict.values()).count(b"=\n") == 2:
+        if b"".join(received_dict.values()).count(b"=\n") == 20:
+            max_key = max(received_dict.keys())
             print(len(received_dict), int(sequence_number) + 1)
-            if len(received_dict) == int(sequence_number) + 1:
+            # print(sorted(received_dict.keys()))
+            if len(received_dict) == int(max_key) + 1:
                 print("break")  # , size, i)
                 completed = True
                 break
-        # received_data += chunk
-        # # continue
-        # if received_data.count(b"=\n") == 20: #20
 
     received_data = b"".join(received_dict[key] for key in sorted(received_dict.keys()))
     print(len(received_data))
@@ -113,54 +95,6 @@ def receive_and_save_objects(client_socket, objects_dir):
         except Exception as e:
             print(f"Error saving {size} Object {file_idx}: {e}")
 
-        # left, mid, right = chunk.partition(b"=\n")
-        # if not mid:
-        #     received_data += chunk
-        # else:
-        #     received_data += left + mid
-        #     print(size, i, len(received_data))
-        #     break
-
-    # for i in range(10):
-    #     completed = False
-    #     for size in "large", "small":
-    #         received_data = right
-    #         while True:
-    #             chunk, _ = client_socket.recvfrom(1024)
-    #             sequence_number, chunk = chunk.decode().split(HEADER_SEPARATOR)
-    #             chunk = chunk.encode()
-    #             print("receive", sequence_number, len(chunk))
-    #             send_acknowledgment(client_socket, sequence_number)
-    #             # continue
-    #             if not chunk:
-    #                 print("break", size, i)
-    #                 completed = True
-    #                 break
-    #             left, mid, right = chunk.partition(b"=\n")
-    #             if not mid:
-    #                 received_data += chunk
-    #             else:
-    #                 received_data += left + mid
-    #                 print(size,i,len(received_data))
-    #                 break
-    #
-    #         if not os.path.exists(objects_dir):
-    #             os.makedirs(objects_dir)
-    #
-    #         file_path = os.path.join(objects_dir, f"received_{size}-{i}.obj")
-    #
-    #         try:
-    #             with open(file_path, "wb") as file:
-    #                 file.write(received_data)
-    #             print(f"Received and saved {size} Object {i} to {file_path}")
-    #
-    #         except Exception as e:
-    #             print(f"Error saving {size} Object {i}: {e}")
-
-
-# except Exception as e:
-#     print(f"An error occurred: {e}")
-
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket:
@@ -170,43 +104,8 @@ def main():
         print("UDP Client is ready...")
         receive_and_save_objects(client_socket, OBJECTS_DIR)
 
-    # except Exception as e:
-    #     print(f"An error occurred: {e}")
+        print("corrupted", corrupted_count)
 
 
 if __name__ == "__main__":
     main()
-
-# def udp_client(file_name):
-#     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#
-#     server_address = (IP_ADDRESS, PORT)
-#
-#     # Send the file name to the server
-#     client_socket.sendto(file_name.encode(), server_address)
-#
-#     file_path = os.path.join(OBJECTS_DIR, file_name)
-#
-#     try:
-#         with open(file_path, "wb") as file:
-#             while True:
-#                 chunk, _ = client_socket.recvfrom(BUFFER_SIZE)
-#                 if not chunk:
-#                     break
-#                 if b"=" in chunk:
-#                     file.write(chunk)
-#                     break
-#                 file.write(chunk)
-#
-#         print(f"Received and saved {file_name} to {file_path}")
-#
-#     except Exception as e:
-#         print(f"An error occurred: {e}")
-#
-#     finally:
-#         client_socket.close()
-#
-#
-# if __name__ == "__main__":
-#     file_name = "small-1.obj"  # Specify the file to be requested
-#     udp_client(file_name)
--- code/udp/udp_server.py
+++ code/udp/udp_server.py
@@ -2,6 +2,9 @@ import socket
 import os
 import time
 import hashlib
+import sys
+
+sys.stdout = open("/dev/null", "w")
 
 IP_ADDRESS = "172.17.0.2"
 PORT = 45404
@@ -13,13 +16,13 @@ EXPERIMENT_LOG_FILE = "server_experiment_log.txt"
 # OBJECTS_DIR = "../../objects/"
 
 
-WINDOW_SIZE = 5
 
-BUFFER_SIZE = 1024  # Adjust the buffer size according to your needs
+WINDOW_SIZE = 100
+
+BUFFER_SIZE = 2048  # Adjust the buffer size according to your needs
 CHECKSUM_SIZE = 16
 SEQUENCE_NUMBER_SIZE = 4
-HEADER_SEPARATOR = ":"
-TIMEOUT = 0.05
+TIMEOUT = 0.15
 
 
 def calculate_checksum(data):
@@ -72,29 +75,18 @@ def send_file(server_socket, file_path, client_address):
                     server_socket, sequence_number, chunk, client_address
                 )
                 sequence_number += 1
-                # server_socket.sendto(f"{sequence_number:04}{HEADER_SEPARATOR}{chunk.decode()}".encode(), client_address)
 
             ack_numbers = receive_acknowledgments(server_socket, window)
             sent_not_acked.difference_update(ack_numbers)
             
             print("ack", ack_numbers)
 
-            # ack_list.extend(ack_numbers)
-            # filtered_list = [
-            #     element for element in set(ack_list) if ack_list.count(element) > 1
-            # ]
 
             for ack in ack_numbers:
                 if (ack) in acked_table:
                     acked_table[(ack)] = 2
                 else:
                     acked_table[(ack)] = 1
-
-            # print("filtered_list", filtered_list)
-            # print("ack_table", acked_table)
-            # print("ack", ack_numbers)
-            # print("window", window.keys())
-            # print("sent_not_acked", sent_not_acked)
 
             for ack_number in ack_numbers:
                 # if ack_number not in window and ack_number in filtered_list:
@@ -106,10 +98,11 @@ def send_file(server_socket, file_path, client_address):
                 #     continue
 
             if not window and not chunk:
+                print("window chunk break")
                 break
 
             for seq in sent_not_acked:
-                if not_acked_counter <= 10:
+                if not_acked_counter <= 150:
                     print("resend", seq)
                     counter += 1
                     send_data_with_reliability(
@@ -138,22 +131,23 @@ def receive_acknowledgments(server_socket, window):
 
 
 def main():
-    # try:
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server_socket.bind((IP_ADDRESS, PORT))
 
         # Wait for the "ready" message from the client
         print("Waiting for the client to be ready...")
-        data, client_address = server_socket.recvfrom(1024)
+        data, client_address = server_socket.recvfrom(BUFFER_SIZE)
         print(f"Received ready message from {client_address}")
+        
+        time.sleep(0.1)
 
         start_time = time.time()
 
         # connection, client_address = server_socket.accept()
         # print(f"Connection from {client_address}")
 
-        for i in range(1):
+        for i in range(10):
             for size in "large", "small":
                 file_path = os.path.join(OBJECTS_DIR, f"{size}-{i}.obj")
 
@@ -167,58 +161,11 @@ def main():
         elapsed_time = end_time - start_time
 
         print(f"Counter: {counter}")
-        # with open(EXPERIMENT_LOG_FILE, "a") as log_file:
-        #     log_file.write(f"Experiment x: {elapsed_time} seconds\n")
+        with open(EXPERIMENT_LOG_FILE, "a") as log_file:
+            log_file.write(f"Experiment x: {elapsed_time} seconds\n")
 
 
-# except Exception as e:
-#     print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
     main()
-
-
-# def udp_server():
-#     # Create a UDP socket
-#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#
-#     # Set the socket options to allow quick reuse of the socket
-#     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
-#
-#     # Bind the socket to the IP address and port
-#     server_socket.bind((IP_ADDRESS, PORT))
-#
-#     print(f"UDP server is listening on {IP_ADDRESS}:{PORT}")
-#
-#     while True:
-#         # Receive data from the client
-#         # The UDP socket will wait here to receive data from the client in idle state
-#         data, client_address = server_socket.recvfrom(BUFFER_SIZE)
-#
-#         # Get the file name from the received data
-#         file_name = data.decode()
-#
-#         # Construct the file path
-#         file_path = os.path.join(OBJECTS_DIR, file_name)
-#
-#         if os.path.exists(file_path):
-#             with open(file_path, "rb") as file:
-#                 # Send the file to the client chunk by chunk
-#                 while True:
-#                     chunk = file.read(BUFFER_SIZE)
-#                     if not chunk:
-#                         break
-#                     # print(chunk[-10:])
-#                     server_socket.sendto(chunk, client_address)
-#
-#                 print(f"Sent {file_name} to {client_address}")
-#         else:
-#             error_message = f"Error: File not found - {file_path}"
-#             server_socket.sendto(error_message.encode(), client_address)
-#             print(error_message)
-#             break  # Stop the server after handling one file
-#
-#
-# if __name__ == "__main__":
-#     udp_server()
