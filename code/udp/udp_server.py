47d6127ef90064052d37ee3d027032b7ffe6cf69
basar-yilmaz <basaryilmaz.tr@outlook.com>
2024-01-05 15:36:50 +0300

    server update

--- code/udp/udp_server.py
+++ code/udp/udp_server.py
@@ -7,49 +7,80 @@ PORT = 45404
 OBJECTS_DIR = "/root/objects/"
 EXPERIMENT_LOG_FILE = "server_experiment_log.txt"
 
+# IP_ADDRESS = "0.0.0.0"
+# PORT = 45404
+# OBJECTS_DIR = "../../objects/"
+
+
 WINDOW_SIZE = 5
 
 BUFFER_SIZE = 1024  # Adjust the buffer size according to your needs
 HEADER_SIZE = 4
 HEADER_SEPARATOR = ":"
-TIMEOUT = 0.1
+TIMEOUT = 0.05
+
 
 def send_data_with_reliability(server_socket, sequence_number, data, client_address):
     print("send", sequence_number, len(data))
-    server_socket.sendto(f"{sequence_number:04}{HEADER_SEPARATOR}{data.decode()}".encode(), client_address)
+    server_socket.sendto(
+        f"{sequence_number:04}{HEADER_SEPARATOR}{data.decode()}".encode(),
+        client_address,
+    )
+
+
+sequence_number = 0
+window = {}
+sent_not_acked = set()
+ack_list = []
 
 
 def send_file(server_socket, file_path, client_address):
     with open(file_path, "rb") as file:
-        sequence_number = 0
-        window = {}
-        sent_not_acked = set()
+        global sequence_number
 
         while True:
             while len(window) < WINDOW_SIZE:
-                chunk = file.read(BUFFER_SIZE - HEADER_SIZE)
-                time.sleep(0.001)
+                chunk = file.read(BUFFER_SIZE - HEADER_SIZE - 1)
+                # time.sleep(0.001)
                 if not chunk:
                     break
                 window[sequence_number] = chunk
                 sent_not_acked.add(sequence_number)
+                send_data_with_reliability(
+                    server_socket, sequence_number, chunk, client_address
+                )
                 sequence_number += 1
-                send_data_with_reliability(server_socket, sequence_number, chunk, client_address)
                 # server_socket.sendto(f"{sequence_number:04}{HEADER_SEPARATOR}{chunk.decode()}".encode(), client_address)
 
             ack_numbers = receive_acknowledgments(server_socket, window)
             sent_not_acked.difference_update(ack_numbers)
 
+            ack_list.extend(ack_numbers)
+            filtered_list = [
+                element for element in set(ack_list) if ack_list.count(element) > 1
+            ]
+
+            print("filtered_list", filtered_list)
+            print("ack", ack_numbers)
+            print("window", window.keys())
+            print("sent_not_acked", sent_not_acked)
+
             for ack_number in ack_numbers:
-                del window[ack_number - 1]
+                if ack_number not in window and ack_number in filtered_list:
+                    continue
+                del window[ack_number]
 
             if not window and not chunk:
                 break
 
             for seq in sent_not_acked:
-                send_data_with_reliability(server_socket, seq, window[seq], client_address)
+                print("resend", seq)
+                send_data_with_reliability(
+                    server_socket, seq, window[seq], client_address
+                )
 
-            time.sleep(TIMEOUT)
+            # time.sleep(TIMEOUT)
+
 
 def receive_acknowledgments(server_socket, window):
     ack_numbers = set()
@@ -68,37 +99,38 @@ def receive_acknowledgments(server_socket, window):
 
 def main():
     # try:
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
-            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            server_socket.bind((IP_ADDRESS, PORT))
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind((IP_ADDRESS, PORT))
 
-            # Wait for the "ready" message from the client
-            print("Waiting for the client to be ready...")
-            data, client_address = server_socket.recvfrom(1024)
-            print(f"Received ready message from {client_address}")
+        # Wait for the "ready" message from the client
+        print("Waiting for the client to be ready...")
+        data, client_address = server_socket.recvfrom(1024)
+        print(f"Received ready message from {client_address}")
 
-            start_time = time.time()
+        start_time = time.time()
 
-            # connection, client_address = server_socket.accept()
-            # print(f"Connection from {client_address}")
+        # connection, client_address = server_socket.accept()
+        # print(f"Connection from {client_address}")
 
-            for i in range(10):
-                for size in "large", "small":
-                    file_path = os.path.join(OBJECTS_DIR, f"{size}-{i}.obj")
+        for i in range(10):
+            for size in "large", "small":
+                file_path = os.path.join(OBJECTS_DIR, f"{size}-{i}.obj")
 
-                    if not os.path.exists(file_path):
-                        print(f"Error: File not found - {file_path}")
-                        continue
-                    send_file(server_socket, file_path, client_address)
-                    print(f"Sent {size} Object {i}")
+                if not os.path.exists(file_path):
+                    print(f"Error: File not found - {file_path}")
+                    continue
+                send_file(server_socket, file_path, client_address)
+                print(f"Sent {size} Object {i}")
 
-            end_time = time.time()
-            elapsed_time = end_time - start_time
-            with open(EXPERIMENT_LOG_FILE, "a") as log_file:
-                log_file.write(f"Experiment x: {elapsed_time} seconds\n")
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        # with open(EXPERIMENT_LOG_FILE, "a") as log_file:
+        #     log_file.write(f"Experiment x: {elapsed_time} seconds\n")
 
-    # except Exception as e:
-    #     print(f"An error occurred: {e}")
+
+# except Exception as e:
+#     print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
